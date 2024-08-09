87aff154c87aec29a8ce196d97e60a6800cb9d77
q-sakakima <sakakima@q-co.jp>
2024-08-09 10:42:10 +0900

    不要なインデントの削除

--- components/Square/index.tsx
+++ components/Square/index.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent } from "react";
 import { Mark } from "../../types/index";
-import { css , ClassNames } from "@emotion/react"; 
+import { css , ClassNames } from "@emotion/react";
 import { square } from "./styled";
 
 type SquareProps = {
