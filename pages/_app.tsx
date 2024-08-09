71fdc40310ab8a1b87ecfbc6b21786a3db02d60d
q-sakakima <sakakima@q-co.jp>
2024-08-09 12:56:41 +0900

    レスポンシブ対応

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/global.css';
+import '../styles/responsive.css';
 import { FunctionComponent, useState } from "react";
 import { Mark } from "../types/index";
 import { Board } from "../components/Board";
