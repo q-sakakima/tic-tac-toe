a2ae2e94a000ff7afe94b864ace9fdf0997b0ac6
q-sakakima <sakakima@q-co.jp>
2024-08-19 15:58:22 +0900

    ダークモードへ切り替えることができるボタンの作成

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { Board } from "../components/Board";
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [bgColor, setBgColor] = useState(false);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
@@ -34,13 +35,22 @@ export default function Game() {
     );
   });
 
+  const handleBGColor = () => {
+    setBgColor(!bgColor);
+  }
+
+  const bgColorClass = bgColor ? 'bg-color-gray' : '';
+
   return (
-    <div className="game">
+    <div className={`game ${bgColorClass}`}>
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
+      </div>
+      <div>
+        <button onClick={() => handleBGColor()}>Change BGColor</button>
       </div>
     </div>
   );
