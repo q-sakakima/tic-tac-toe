205d49e5de1893c7497f6b20a278f4e028a76326
q-sakakima <sakakima@q-co.jp>
2024-08-26 13:04:23 +0900

    memoの導入

--- components/Board/index.tsx
+++ components/Board/index.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useContext, useMemo } from 'react';
+import { FunctionComponent, memo, useContext, useMemo } from 'react';
 import { ResultCheckContext } from '../../contexts/ResultCheckProvider';
 import { Mark, Coordinates } from '../../types/index';
 import { Square } from '../Square';
@@ -13,87 +13,83 @@ export type BoardProps = {
   handlePlay: (nextSquares: Mark[], coordinates: Coordinates) => void;
 };
 
-export const Board: FunctionComponent<BoardProps> = ({
-  squares,
-  lines,
-  boardSize,
-  timeLeft,
-  handlePlay,
-}: BoardProps) => {
-  const { isWin, setIsWin, isDraw, setIsDraw, xIsNext, setXIsNext } =
-    useContext(ResultCheckContext);
-  let winnersSquares: boolean[] = Array(boardSize ** 2).fill(false);
-  let coordinates: Coordinates[] = [];
+export const Board: FunctionComponent<BoardProps> = memo(
+  ({ squares, lines, boardSize, timeLeft, handlePlay }: BoardProps) => {
+    const { isWin, setIsWin, isDraw, setIsDraw, xIsNext, setXIsNext } =
+      useContext(ResultCheckContext);
+    let winnersSquares: boolean[] = Array(boardSize ** 2).fill(false);
+    let coordinates: Coordinates[] = [];
 
-  for (let i = 1; i <= boardSize ** 2; i++) {
-    for (let j = 1; j <= boardSize ** 2; j++) {
-      coordinates.push({ x: j, y: i });
-    }
-  }
-
-  const calculateWinner = (squares: Mark[]) => {
-    for (let i = 0; i < lines.length; i++) {
-      const line = lines[i].slice();
-      if (line.every((v) => squares[v] && squares[v] === squares[line[0]])) {
-        line.forEach((v) => {
-          winnersSquares[v] = true;
-          setIsWin(true);
-        });
-        return squares[line[0]];
+    for (let i = 1; i <= boardSize ** 2; i++) {
+      for (let j = 1; j <= boardSize ** 2; j++) {
+        coordinates.push({ x: j, y: i });
       }
     }
-    return null;
-  };
 
-  const handleClick = (i: number) => {
-    if (isWin !== null || isDraw || squares[i] || !timeLeft) {
-      return;
-    }
-    const nextSquares: Mark[] = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
+    const calculateWinner = (squares: Mark[]) => {
+      for (let i = 0; i < lines.length; i++) {
+        const line = lines[i].slice();
+        if (line.every((v) => squares[v] && squares[v] === squares[line[0]])) {
+          line.forEach((v) => {
+            winnersSquares[v] = true;
+            setIsWin(true);
+          });
+          return squares[line[0]];
+        }
+      }
+      return null;
+    };
+
+    const handleClick = (i: number) => {
+      if (isWin !== null || isDraw || squares[i] || !timeLeft) {
+        return;
+      }
+      const nextSquares: Mark[] = squares.slice();
+      if (xIsNext) {
+        nextSquares[i] = 'X';
+      } else {
+        nextSquares[i] = 'O';
+      }
+      handlePlay(nextSquares, coordinates[i]);
+    };
+
+    const winner = calculateWinner(squares);
+    let status: string;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else if (isDraw) {
+      status = 'Draw';
+    } else if (isWin === false) {
+      status = 'YOU LOSE: ' + (xIsNext ? 'X' : 'O');
     } else {
-      nextSquares[i] = 'O';
+      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
     }
-    handlePlay(nextSquares, coordinates[i]);
-  };
 
-  const winner = calculateWinner(squares);
-  let status: string;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else if (isDraw) {
-    status = 'Draw';
-  } else if (isWin === false) {
-    status = 'YOU LOSE: ' + (xIsNext ? 'X' : 'O');
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-  }
-
-  return (
-    <ClassNames>
-      {({ css }) => (
-        <>
-          <div className={css(status)}>{status}</div>
-          <div
-            className={css`
-              ${boardRow};
-              ${boardSize === 3 ? boardRow3x3 : boardRow4x4};
-            `}
-          >
-            {[...Array(boardSize ** 2)].map((_, i) => {
-              return (
-                <Square
-                  key={i}
-                  value={squares[i]}
-                  winnersSquare={winnersSquares[i]}
-                  onSquareClick={() => handleClick(i)}
-                />
-              );
-            })}
-          </div>
-        </>
-      )}
-    </ClassNames>
-  );
-};
+    return (
+      <ClassNames>
+        {({ css }) => (
+          <>
+            <div className={css(status)}>{status}</div>
+            <div
+              className={css`
+                ${boardRow};
+                ${boardSize === 3 ? boardRow3x3 : boardRow4x4};
+              `}
+            >
+              {[...Array(boardSize ** 2)].map((_, i) => {
+                return (
+                  <Square
+                    key={i}
+                    value={squares[i]}
+                    winnersSquare={winnersSquares[i]}
+                    onSquareClick={() => handleClick(i)}
+                  />
+                );
+              })}
+            </div>
+          </>
+        )}
+      </ClassNames>
+    );
+  },
+);
