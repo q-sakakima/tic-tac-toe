198e294876f8026eb967ded203265b8f62f446b9
q-sakakima <sakakima@q-co.jp>
2024-08-26 13:15:52 +0900

    lines変数にuseMemoを導入

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,4 +1,10 @@
-import { FunctionComponent, useState, useEffect, useContext } from 'react';
+import {
+  FunctionComponent,
+  useState,
+  useEffect,
+  useContext,
+  useMemo,
+} from 'react';
 import { ResultCheckContext } from '../contexts/ResultCheckProvider';
 import { Mark, Coordinates } from '../types/index';
 import { Board } from '../components/Board';
@@ -12,36 +18,38 @@ export default function Game() {
   const [boardSize, setBoardSize] = useState<number>(3);
   const currentSquares = history[currentMove]?.squares;
 
-  let lines: number[][] = [];
   const { isWin, setIsWin, isDraw, setIsDraw, xIsNext, setXIsNext } =
     useContext(ResultCheckContext);
   const [timeLeft, setTimeLeft] = useState<number>(10);
 
-  if (boardSize === 3) {
-    lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-  } else if (boardSize === 4) {
-    lines = [
-      [0, 1, 2, 3],
-      [4, 5, 6, 7],
-      [8, 9, 10, 11],
-      [12, 13, 14, 15],
-      [0, 4, 8, 12],
-      [1, 5, 9, 13],
-      [2, 6, 10, 14],
-      [3, 7, 11, 15],
-      [0, 5, 10, 15],
-      [3, 6, 9, 12],
-    ];
-  }
+  const lines: number[][] = useMemo(() => {
+    if (boardSize === 3) {
+      return [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+    } else if (boardSize === 4) {
+      return [
+        [0, 1, 2, 3],
+        [4, 5, 6, 7],
+        [8, 9, 10, 11],
+        [12, 13, 14, 15],
+        [0, 4, 8, 12],
+        [1, 5, 9, 13],
+        [2, 6, 10, 14],
+        [3, 7, 11, 15],
+        [0, 5, 10, 15],
+        [3, 6, 9, 12],
+      ];
+    }
+    return [];
+  }, [boardSize]);
 
   useEffect(() => {
     const checkDraw = lines.map((line) => {
